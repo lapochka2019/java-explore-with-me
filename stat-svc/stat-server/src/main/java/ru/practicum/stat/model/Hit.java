0d60bdb7256beea13993213bdbcc7fcb9e1fd200
Anstasia <lapteva_02@list.ru>
2025-07-06 15:04:41 +0300

    stat-service (stats-svc) Изменено имя поля timestamp -> created Скорректированы запросы в БД

---
+++ stat-svc/stat-server/src/main/java/ru/practicum/stat/model/Hit.java
@@ -0,0 +1,27 @@
+package ru.practicum.stat.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "statistic")
+public class Hit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String app;
+    @Column(nullable = false)
+    private String uri;
+    @Column(nullable = false)
+    private String ip;
+    @Column(nullable = false)
+    private LocalDateTime created;
+}
