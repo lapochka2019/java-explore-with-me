d21dd03d464f0c452f57163b6af762294ff7a522
Anstasia <lapteva_02@list.ru>
2025-07-21 13:45:00 +0300

    location + тесты

---
+++ ewm-main-svc/src/main/java/ru/practicum/ewm/location/dto/LocationMapper.java
@@ -0,0 +1,12 @@
+package ru.practicum.ewm.location.dto;
+
+import org.mapstruct.Mapper;
+import ru.practicum.ewm.location.model.Location;
+
+@Mapper(componentModel = "spring")
+public interface LocationMapper {
+
+    Location toLocation (LocationDto locationDto);
+
+    LocationDto toLocationDto (Location location);
+}
